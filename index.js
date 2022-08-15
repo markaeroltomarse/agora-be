1a80e6a021cd89a82cc47678c6c8816ea31f2d26
markaeroltomarse <marktomarse@gmail.com>
2022-08-15 16:01:54 +0800

    Adding agora

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
+const fs = require('fs/promises')
+const path = require('path')
 const express = require('express')
-
 const app = express()
 
-
 const { errorHandler } = require('./middleware/errors')
-//const winston = require('winston');
+const winston = require('winston');
 
+// Logs
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
 
-//Logs 
-// const logger = winston.createLogger({
-//   level: 'info',
-//   format: winston.format.json(),
-//   defaultMeta: { service: 'user-service' },
-//   transports: [
-//     //
-//     // - Write all logs with importance level of `error` or less to `error.log`
-//     // - Write all logs with importance level of `info` or less to `combined.log`
-//     //
-//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-//     new winston.transports.File({ filename: 'combined.log' }),
-//   ],
-// });
-
-// if (process.env.NODE_ENV !== 'production') {
-//     logger.add(new winston.transports.Console({
-//         format: winston.format.simple(),
-//     }));
-// }
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
 
 app.use((req, res, next) => {
-    // logger.log({
-    //     level: 'info',
-    //     message: `${req.hostname}: ${req.method}`
-    // });
+    logger.log({
+        level: 'info',
+        message: `${req.hostname}: ${req.method}`
+    });
 
     next()
 })
@@ -60,7 +59,19 @@ app.use('/api/room', require('./components/room/room.route'))
 // Error handler
 app.use(errorHandler)
 
+// AGORA 
 
+app.get('/AgoraRTM', async (req, res) => {
+    const AgoraRTM = await fs.readFile(path.join(__dirname, '/services/agora-rtm-sdk-1.4.5.js'))
+    res.type('.js')
+    res.send(AgoraRTM.toString())
+})
+
+app.get('/AgoraRTC', async (req, res) => {
+    const AgoraRTC = await fs.readFile(path.join(__dirname, '/services/AgoraRTC_N-4.13.0.js'))
+    res.type('.js')
+    res.send(AgoraRTC.toString())
+})
 
 // Start the app
 const PORT = process.env.PORT || 3001
